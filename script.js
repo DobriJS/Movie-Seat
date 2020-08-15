e72cb976e0f87c65ef51a07f1f0c03bc8b8e93ff
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-08-15 18:23:30 +0300

    Seats click Event

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 const ticketPrice = Number(movieSelect.value);
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Seat click event
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+    }
+
+    updateSelectedCount()
+});
