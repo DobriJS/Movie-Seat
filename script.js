13cb2566eac49304ea7c9f284fb9b6f0340fa6e2
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2020-08-15 19:34:34 +0300

    Populate UI With Saved Data

--- script.js
+++ script.js
@@ -26,6 +26,25 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = Number(e.target.value);
